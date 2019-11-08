ad6e9b8e879ee179314a9bf1a12169a2d28fffdf
JordiRomeroUB <31921529+JordiRomeroUB@users.noreply.github.com>
2019-11-08 18:56:07 +0100

    Add files via upload

--- P3/src/plantilla-menu.c
+++ P3/src/plantilla-menu.c
@@ -7,11 +7,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include <ctype.h>  
-#include "red-black-tree.h"
-#include "tree_operations.h"
 
 #define MAXCHAR      100
 #define MAGIC_NUMBER 0x01234567
@@ -49,10 +45,18 @@ int menu()
 
 int main(int argc, char **argv)
 {
+    char * line = NULL;
+    size_t len = 0;
+    size_t read;
+    int magicNumber = MAGIC_NUMBER;
+    unsigned char buffer[10];
     char str1[MAXCHAR], str2[MAXCHAR];
     int opcio;
-    rb_tree *tree;
-
+    int num;
+    FILE *fptr;
+    FILE *ptr;    
+    
+    
     if (argc != 1)
         printf("Opcions de la linia de comandes ignorades\n");
 
@@ -71,17 +75,22 @@ int main(int argc, char **argv)
                 printf("Fitxer de base de dades: ");
                 fgets(str2, MAXCHAR, stdin);
                 str2[strlen(str2)-1]=0;
-                tree = createTree(str1,str2);
-                printf("the: %d",find_node(tree,"the")->num_times);
+
+                /* Falta codi */
+
                 break;
 
             case 2:
                 printf("Nom de fitxer en que es desara l'arbre: ");
                 fgets(str1, MAXCHAR, stdin);
                 str1[strlen(str1)-1]=0;
-
-                /* Falta codi */
-
+                ptr = fopen(str1, "w"); // w for write, b for binary
+                if(ptr == NULL){
+                    printf("Error!");   
+                    exit(1);             
+                }
+                fwrite(&magicNumber,sizeof(int), 1,ptr); // write 10 bytes from our buffer
+                fclose(ptr);
                 break;
 
             case 3:
@@ -89,8 +98,24 @@ int main(int argc, char **argv)
                 fgets(str1, MAXCHAR, stdin);
                 str1[strlen(str1)-1]=0;
 
-                /* Falta codi */
+                //ptr = fopen("prova.bin","rb");  // r for read, b for binary
+                //fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
 
+                
+
+                fp = fopen(str1, "r");
+                if (fp == NULL)
+                    exit(EXIT_FAILURE);
+
+                while ((read = getline(&line, &len, fp)) != -1) {
+                    printf("Retrieved line of length %zu:\n", read);
+                    printf("%s", line);
+                }
+
+                fclose(fp);
+                if (line)
+                    free(line);
+                exit(EXIT_SUCCESS);
                 break;
 
             case 4:
